e42ae6f3c85f7e0874de1d96c675353598e5f283
Steini3008 <nicolas.steinbach@yahoo.de>
2024-11-28 18:25:55 +0100

    Improved with 'multiplication' and 'division'

--- src/SimpleMath.java
+++ src/SimpleMath.java
@@ -7,4 +7,13 @@ public class SimpleMath {
 	public int subtraction(int a, int b) {
 		return a-b;
 	}
+	
+	public int multiplication(int a, int b) {
+		return a*b;
+	}
+	
+	public int division(int a, int b) {
+		return a/b;
+	}
+	
 }
